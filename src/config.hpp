19764ae8aaa1c27048a9c4be39f2231f0d43b1b9
Milan Tol <milantol35@gmail.com>
2026-01-05 19:03:02 +0100

    .

--- src/config.hpp
+++ src/config.hpp
@@ -5,7 +5,7 @@ namespace cfg
 {
 
 sf::Vector2u const window_size = {1000, 1000};
-sf::Vector2i const grid_size = {3, 3};
+sf::Vector2i const grid_size = {4, 4};
 
 float const cross_radius = 100;
 float const circle_radius = 100;
--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,12 @@
 
 #include "config.hpp"
 
-#include "board/square.hpp"
-
-
 Input input;
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({cfg::window_size.x, cfg::window_size.y}), "Let's play tictactoe!");
     window.setFramerateLimit(60);
-
-    std::vector<std::vector<Square>> boegreSid;
-    
-    for (int column = 0; column < cfg::grid_size.x; column++)
-    {   
-        std::vector<Square> column_vector;
-        boegreSid.push_back(column_vector);
-
-        for (int row = 0; row < cfg::grid_size.y; row++)
-        {   
-            column_vector.push_back(Square(column, row));
-            boegreSid[column][row] = Square(column, row);
-        }
-    }
 
     while (window.isOpen())
     {
